a12e845eb0530a863bb779d3b64f2ae85a64632c
CommanderRedYT <ccommanderred@gmail.com>
2026-01-21 23:39:59 +0100

    Improve exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-export { parseLevelInitData, parseSaveFile } from '@/parser';
+import { parseLevelInitData, parseSaveFile } from '@/parser';
+
+export { parseLevelInitData, parseSaveFile };
+
+export default {
+    parseLevelInitData,
+    parseSaveFile,
+};
